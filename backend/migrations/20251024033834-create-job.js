eb3ce6626f96e35fb48fef5467d70f9fac9d2418
Dhanushri26 <ds20262005@gmail.com>
2025-10-24 09:39:07 +0530

    done job table

---
+++ backend/migrations/20251024033834-create-job.js
@@ -0,0 +1,55 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Jobs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      requirements: {
+        type: Sequelize.TEXT
+      },
+      salary: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      remote: {
+        type: Sequelize.BOOLEAN
+      },
+      experience: {
+        type: Sequelize.STRING
+      },
+      tags: {
+        type: Sequelize.STRING
+      },
+      benefits: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Jobs');
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
 const userRoutes = require("./routes/user.routes");
 app.use("/api/users", userRoutes);
 
-// Optional other routes if ready
-// const jobRoutes = require("./routes/jobPosting.routes");
-// const skillGapRoutes = require("./routes/skillGap.routes");
-// app.use("/api/jobs", jobRoutes);
-// app.use("/api/skills", skillGapRoutes);
+const jobPostingRoutes = require("./routes/jobPosting.routes");
+app.use("/api/jobs", jobPostingRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
